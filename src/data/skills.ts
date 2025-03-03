85bbb28a727f63287cca8c6d709d8068a4c7d9a0
duck <gukgyeon@gmail.com>
2025-03-03 13:13:25 +0900

    update
    update project

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -3,5 +3,5 @@ export const skills = {
     languages: ["Python", "Matlab", "C/C++", "Java", "JavaScript", "jQuery", "Ajax"],
     frameworks: ["TensorFlow", "PyTorch", "Transformers", "Scikit-Learn", "OpenCV", "FastAPI", "Flask", "Celery", "Next.js"],
     tools: ["PyCharm", "Visual Studio Code", "Sublime Text", "Vim", "Claude", "Docker"],
-    databases: ["MongoDB", "Redis", "Oracle", "MySQL"]
+    databases: ["Oracle", "MySQL", "MongoDB", "FAISS", "Redis"]
   };
